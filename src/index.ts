4ed0ec55e883e5a062e976ad8978b60db3cb7c8b
d3rpp <hudson.curren@icloud.com>
2023-03-08 15:37:41 +1300

    formatted & optimised

--- src/index.ts
+++ src/index.ts
@@ -3,36 +3,26 @@ const SLUG = /^[a-z0-9]+(?:-[a-z0-9]+)*$/g;
 const DOMAIN = /^https?\:\/\/([a-z0-9].?)+\//;
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-		let path = request.url.replace(DOMAIN, "");
+	async fetch(request: Request, env: Env, _ctx: ExecutionContext) {
+		let path = request.url.replace(DOMAIN, '');
 
 		// Root URL
-		if (path == "" ) {
-			return new Response(null, {
-				headers: {
-					"Location": "https://d3rpp.dev"
-				},
-				status: 302
-			})
+		if (path == '') {
+			return Response.redirect('https://d3rpp.dev');
 		}
 
 		if (SLUG.test(path)) {
 			// tested to be slug, valid
-			let return_value = await env.URLS.get(path, "text");
+			let return_value = await env.URLS.get(path, 'text');
 
 			if (return_value === null) {
-				return new Response("Not Found", { status: 404 });
+				return new Response(null, { status: 404 });
 			} else {
-				return new Response("Found", {
-					headers: {
-						"Location": return_value
-					},
-					status: 302
-				});
+				return Response.redirect(return_value);
 			}
 		} else {
 			// invalid slug
-			return new Response("Bad Request", {status: 400})
+			return new Response('Bad Request', { status: 400 });
 		}
-	}
-}
+	},
+};
